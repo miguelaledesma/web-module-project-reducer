796f9f1cc0190a21074a8bf26105445228eafdf5
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-21 14:27:56 -0700

    calulcator is showing up on local host and showing the 100 form state

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
 import './App.css';
-
+import reducer, {initialState} from '../reducers';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
 function App() {
+  const [ state, dispatch ] = useReducer(reducer, initialState)
+  console.log(state)
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -16,10 +18,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation} </span>
+              <span id="memory"><b>Memory:</b> {state.memory} </span>
             </div>
             
             <div className="row">
