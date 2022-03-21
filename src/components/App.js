b7273e3137e576eecf322088588711af1818f854
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-21 14:42:04 -0700

    adding my onclick functions to each number

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,20 @@ import './App.css';
 import reducer, {initialState} from '../reducers';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
+import {applyNumber} from '../actions'
+
+
+
 
 function App() {
   const [ state, dispatch ] = useReducer(reducer, initialState)
   console.log(state)
+
+  const handleNumberClick = (number) => {
+    dispatch(applyNumber(number))
+  }
+
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -31,21 +41,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick = {() => handleNumberClick(1)}/>
+              <CalcButton value={2} onClick = {() => handleNumberClick(2)}/>
+              <CalcButton value={3} onClick = {() => handleNumberClick(3)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick = {() => handleNumberClick(4)}/>
+              <CalcButton value={5} onClick = {() => handleNumberClick(5)}/>
+              <CalcButton value={6} onClick = {() => handleNumberClick(6)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick = {() => handleNumberClick(7)}/>
+              <CalcButton value={8} onClick = {() => handleNumberClick(8)}/>
+              <CalcButton value={9} onClick = {() => handleNumberClick(9)}/>
             </div>
 
             <div className="row">
